87d06920bc133153d500c145908a110ca6854b06
zhk1127 <zhk1127@gmail.com>
2026-05-23 20:55:57 -0400

    Added projects and contact pages

--- module_1/run.py
+++ module_1/run.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
